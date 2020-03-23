99c65b04c22726a25ba4c9dc0deaa65321b2744a
Nastya <nastyasischuk@gmail.com>
2020-03-23 21:44:19 +0300

    added footer

--- src/components/base/Footer.js
+++ src/components/base/Footer.js
@@ -5,8 +5,8 @@ import Menu from "./footer/Menuu";
 class Footer extends Component {
     render() {
         return (
-            <div className="footer color-container-1">
-
+            <div className="footer ">
+            <p>&#169; makeup 2019</p>
             </div>
         );
     }
